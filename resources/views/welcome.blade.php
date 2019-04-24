c8264444bfed1a70f2153682f5d6c3c8070de233
Felipe Flor <f.flor@vehikl.com>
2019-04-24 17:28:16 -0400

    Pre-work (part 2/3): Wire-up regular vue
    Laravel comes with an example vue component ready to go, but the starting Blade template does not reference the compiled javascript (nor css) file at all. This commit will add those references to the Welcome.blade.php template.
    
    Another important point is that the global axios object tries to fetch the csrf_token from a meta tag called 'csrf-token'. However, this tag is not initially present on the Welcome.blade.php template either. So this commit also adds that.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,16 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{csrf_token()}}">
 
     <title>Laravel</title>
+
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+    <script src="{{mix('js/app.js')}}" defer></script>
 </head>
 <body>
 <div id="app">
+    <example-component></example-component>
 </div>
 </body>
 </html>
